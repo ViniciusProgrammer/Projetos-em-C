792de68f1a13c859e7c6885478cfdc2ee8ba24d3
Vinicius Davi <viniprogrammer@hotmail.com>
2023-01-07 09:05:47 -0300

    Arrays

--- Jogo02.c
+++ Jogo02.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+//Trabalhando com Arrays(listas) em C.
+
 int main()
 {
-    
-    
+    int notas [10];
+
+    notas [1] = 10;
+    notas [0] = 5;
+    notas [3] = 2;
+
+    printf("%d %d %d\n", notas[0], notas[1], notas[3]);
+
+
     return 0;
 }
